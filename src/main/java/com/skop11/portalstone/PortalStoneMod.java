d16d1d7c617510bf83328724018dd11446c04b93
skop11 <dbp038@gmail.com>
2016-12-21 17:19:38 +0100

    Skop XI, the derp (CurseForge derp)

--- src/main/java/com/skop11/portalstone/PortalStoneMod.java
+++ src/main/java/com/skop11/portalstone/PortalStoneMod.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class PortalStoneMod
 {
     public static final String MODID = "portalstone";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
     public static final String NAME = "PortalStone Mod";
 
     @SidedProxy(clientSide = "com.skop11.portalstone.proxy.ClientProxy", serverSide = "com.skop11.portalstone.proxy.CommonProxy")
